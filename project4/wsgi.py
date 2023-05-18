b26677e7cead7d46f2ab3bc7611760de71b1d185
greisonrohr <greisonrohr@gmail.com>
2023-05-18 00:06:29 -0300

    azure

--- project4/wsgi.py
+++ project4/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
-import os
 import dotenv
+import os
+
+
+from django.core.wsgi import get_wsgi_application
+
 dotenv.load_dotenv()
 
 
