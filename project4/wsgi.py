11cbf078ebe7b7702d502186f74a7e04de2c1c5c
greisonrohr <greisonrohr@gmail.com>
2023-05-18 00:00:23 -0300

    azure

--- project4/wsgi.py
+++ project4/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+import dotenv
+dotenv.load_dotenv()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project4.settings')
 
