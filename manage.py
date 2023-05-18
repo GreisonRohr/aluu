869f6284464d592edd09bb66aba14fabbfdc6230
greisonrohr <greisonrohr@gmail.com>
2023-05-17 23:50:26 -0300

    azure

--- manage.py
+++ manage.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
 
 def main():
+    # Define a variável de ambiente DJANGO_SETTINGS_MODULE para especificar o arquivo de configurações do projeto Django.
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project4.settings')
     try:
+        # Tenta importar a função execute_from_command_line do módulo django.core.management
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Não foi possível importar o Django. Tem certeza de que ele está instalado e "
+            "disponível em sua variável de ambiente PYTHONPATH? Você "
+            "esqueceu de ativar um ambiente virtual?"
         ) from exc
+    # Chama a função execute_from_command_line com os argumentos da linha de comando (sys.argv) para iniciar o aplicativo Django.
     execute_from_command_line(sys.argv)
 
 
