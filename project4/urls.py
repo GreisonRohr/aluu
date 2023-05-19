a041c256368c3f7485f46788789e8175e020964b
greisonrohr <greisonrohr@gmail.com>
2023-05-19 12:56:32 -0300

    azure

--- project4/urls.py
+++ project4/urls.py
@@ -22,6 +22,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("network.urls")),
-    path("", include("project4.urls")),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
