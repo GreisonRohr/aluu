6605e8c5b5d80248f45e03ae673e67b2277617d5
greisonrohr <greisonrohr@gmail.com>
2023-05-19 00:17:41 -0300

    azure

--- project4/settings.py
+++ project4/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'network/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/network/media')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'network/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/network/static'),
 
 ]
