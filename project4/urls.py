f3ab26458a95fd54ca76d612b05df6d84c8e7cd2
greisonrohr <greisonrohr@gmail.com>
2023-05-20 09:59:59 -0300

    comentários

--- project4/urls.py
+++ project4/urls.py
@@ -1,18 +1,3 @@
-"""project4 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,5 +7,20 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("network.urls")),
-
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Configuração das URLs do Django
+
+# Define a URL para acessar a interface administrativa
+urlpatterns += [
+    path("admin/", admin.site.urls),
+]
+
+# Define a URL raiz para incluir as URLs do aplicativo "network"
+urlpatterns += [
+    path("", include("network.urls")),
+]
+
+# Adiciona a configuração para servir arquivos estáticos (CSS, JS, imagens, etc.)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- project4/wsgi.py
+++ project4/wsgi.py
@@ -1,21 +1,20 @@
-"""
-WSGI config for project4 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
 
 import dotenv
 import os
-
 
 from django.core.wsgi import get_wsgi_application
 
 dotenv.load_dotenv()
 
+# Configuração do arquivo .env
+# Carrega as variáveis de ambiente definidas no arquivo .env
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project4.settings')
 
+# Define a variável de ambiente 'DJANGO_SETTINGS_MODULE' com o valor 'project4.settings'.
+# Essa variável define qual arquivo de configuração do Django será usado.
+
 application = get_wsgi_application()
+
+# Obtém a aplicação WSGI do Django para ser usada pelo servidor web.
+# Essa aplicação é responsável por lidar com as solicitações HTTP recebidas pelo servidor.
