ecc122b4202560efc8d0da7a0e52321c38947e47
greisonrohr <greisonrohr@gmail.com>
2023-05-23 15:09:58 -0300

    atualiza média

--- network/views.py
+++ network/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 
 from django.db.models import Avg
 import json
