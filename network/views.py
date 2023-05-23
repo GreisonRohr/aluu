3d39356d336cd052d69b492a4995970741d337d7
greisonrohr <greisonrohr@gmail.com>
2023-05-23 12:37:13 -0300

    verificar login para avaliar

--- network/views.py
+++ network/views.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
+
+from django.db.models import Avg
 import json
 
 from .models import *
