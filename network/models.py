ee2c680a16ae63153d1988fcb5348f0232d534d5
greisonrohr <greisonrohr@gmail.com>
2023-05-23 09:07:22 -0300

    atualizando nota

--- network/models.py
+++ network/models.py
@@ -20,32 +20,17 @@ class User(AbstractUser):
             "last_name": self.last_name
         }
 
-
-class Rating(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings')
-    rater = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings_given')
-    value = models.DecimalField(max_digits=3, decimal_places=1)
-    created_at = models.DateTimeField(default=timezone.now)
-
-    class Meta:
-        unique_together = [['post', 'rater']]  # To ensure each user can rate a post only once
-
-    def __str__(self):
-        return f"Post: {self.post} | Rater: {self.rater} | Value: {self.value}"
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        self.post.update_rating_average()
-
-class Post(models.Model):
-    creater = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    class Post(models.Model):
+    creater = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
     date_created = models.DateTimeField(default=timezone.now)
     content_text = models.TextField(max_length=140, blank=True)
     content_image = models.ImageField(upload_to='posts/', blank=True)
     likers = models.ManyToManyField(User, blank=True, related_name='likes')
     savers = models.ManyToManyField(User, blank=True, related_name='saved')
     comment_count = models.IntegerField(default=0)
-    rating_average = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    rating_average = models.DecimalField(
+        max_digits=3, decimal_places=1, null=True, blank=True)
 
     def __str__(self):
         return f"Post ID: {self.id} (Creator: {self.creater})"
@@ -62,6 +47,24 @@ class Post(models.Model):
         self.save()
 
 
+class Rating(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='ratings')
+    rater = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='ratings_given')
+    value = models.DecimalField(max_digits=3, decimal_places=1)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        # To ensure each user can rate a post only once
+        unique_together = [['post', 'rater']]
+
+    def __str__(self):
+        return f"Post: {self.post} | Rater: {self.rater} | Value: {self.value}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.post.update_rating_average()
 
 
 class Comment(models.Model):
