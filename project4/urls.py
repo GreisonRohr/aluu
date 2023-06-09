1675bde386cdea4ebabed1d1877e623674ea4ce0
greisonrohr <greisonrohr@gmail.com>
2023-06-08 21:50:14 -0300

    migrações para o banco

--- project4/urls.py
+++ project4/urls.py
@@ -7,19 +7,8 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("network.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Configuração das URLs do Django
-
-# Define a URL para acessar a interface administrativa
-urlpatterns += [
-    path("admin/", admin.site.urls),
 ]
 
-# Define a URL raiz para incluir as URLs do aplicativo "network"
-urlpatterns += [
-    path("", include("network.urls")),
-]
 
 # Adiciona a configuração para servir arquivos estáticos (CSS, JS, imagens, etc.)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
