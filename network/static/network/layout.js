870f7ea15b9fac9ada53d515f13767e5837bc18b
Greison Rohr <greisonrohr@gmail.com>
2023-09-04 20:56:23 -0300

    ajutando head heading

--- network/static/network/layout.js
+++ network/static/network/layout.js
@@ -680,19 +680,3 @@ function goto_register() {
 function goto_login() {
     window.location.href = '/n/login';
 }
-
-
-
-        // Função para alternar o estado do sidenav
-        function toggleSidenav() {
-            var sidenav = document.querySelector('.sidenav');
-            sidenav.classList.toggle('open'); // Adicione ou remova a classe 'open' no sidenav
-        }
-
-        // Seletor para o botão "Menu"
-        var menuToggle = document.getElementById('menu-toggle');
-
-        // Adicione um ouvinte de evento de clique para o botão "Menu"
-        menuToggle.addEventListener('click', function () {
-            toggleSidenav(); // Chame a função toggleSidenav() ao clicar no botão
-        });
